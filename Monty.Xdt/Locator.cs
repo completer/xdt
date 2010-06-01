d855ab934f30806315750c8f1c5c92ec2ce2fdf9
sumo.the.cat <sumo.the.cat@3a2e4736-875a-60fb-4bd3-e3a832ad883a>
2010-06-01 12:40:03 +0000

    small refactorings

--- Monty.Xdt/Locator.cs
+++ Monty.Xdt/Locator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Monty.Xdt
 {
@@ -9,5 +10,19 @@ namespace Monty.Xdt
     {
         public string Kind { get; set; }
         public string Value { get; set; }
+
+        public static Locator Parse(string locator)
+        {
+            var match = Regex.Match(locator, @"(\w*)\((.*)\)");
+
+            if (!match.Success)
+                throw new InvalidOperationException(String.Format("Invalid Locator attribute '{0}'.", locator));
+
+            return new Locator
+            {
+                Kind = match.Groups[1].Value,
+                Value = match.Groups[2].Value
+            };
+        }
     }
 }

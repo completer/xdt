6497e5f32fa2e0636a83ad2eaceb1d16bdf3481d
sumo.the.cat <sumo.the.cat@3a2e4736-875a-60fb-4bd3-e3a832ad883a>
2010-08-12 15:55:09 +0000

    support multiple located elements; remove unwanted xdt attributes from Replace transform; more tests

---
+++ trunk/Monty.Xdt.Test/LocatorTestsByMobius.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Monty.Xdt.Test
+{
+    /// <summary>
+    /// Locator tests based on http://blog.hmobius.com/post/2010/02/17/ASPNET-40-Part-4-Config-Transformation-Files.aspx
+    /// </summary>
+    [TestClass]
+    public class LocatorTestsByMobius
+    {
+        public TestContext TestContext { get; set; }
+
+        XDocument inputDoc;
+        XDocument desiredDoc;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.inputDoc = XDocument.Parse(@"
+                <configuration>
+                  <system.net>
+                    <mailSettings>
+                      <smtp deliveryMethod=""SpecifiedPickupDirectory"" from=""dan@test.com"">
+                        <specifiedPickupDirectory pickupDirectoryLocation=""D:\Temp""/>
+                      </smtp>
+                    </mailSettings>
+                  </system.net>
+                </configuration>
+                ");
+
+            this.desiredDoc = XDocument.Parse(@"
+                <configuration>
+                  <system.net>
+                    <mailSettings>
+                      <smtp deliveryMethod=""Network"" from=""dan@test.com"">
+                        <network host=""liveMailServer"" />
+                      </smtp>
+                    </mailSettings>
+                  </system.net>
+                </configuration>
+                ");
+        }
+
+        [TestMethod]
+        public void TestConditionLocator()
+        {
+            var transformDoc = XDocument.Parse(@"
+                <configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform"">
+                  <system.net>
+                    <mailSettings>
+                      <smtp deliveryMethod=""Network"" from=""dan@test.com""
+                            xdt:Locator=""Condition(@deliveryMethod='SpecifiedPickupDirectory')""
+                            xdt:Transform=""Replace"">
+                        <network host=""liveMailServer""/>
+                      </smtp>
+                    </mailSettings>
+                  </system.net>
+                </configuration>
+                ");
+
+            var output = new XdtTransformer().Transform(this.inputDoc, transformDoc);
+            Assert.IsTrue(XNode.DeepEquals(this.desiredDoc, output));
+        }
+
+        [TestMethod]
+        public void TestMatchLocator()
+        {
+            var transformDoc = XDocument.Parse(@"
+                <configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform"">
+                  <system.net>
+                    <mailSettings>
+                      <smtp deliveryMethod=""Network"" from=""dan@test.com""
+                            xdt:Locator=""Match(from)""
+                            xdt:Transform=""Replace"">
+                        <network host=""liveMailServer""/>
+                      </smtp>
+                    </mailSettings>
+                  </system.net>
+                </configuration>
+                ");
+
+            var output = new XdtTransformer().Transform(this.inputDoc, transformDoc);
+            Assert.IsTrue(XNode.DeepEquals(this.desiredDoc, output));
+        }
+
+        [TestMethod]
+        [Ignore] // todo: enable when XPath is supported
+        public void TestXPathLocator()
+        {
+            var transformDoc = XDocument.Parse(@"
+                <configuration xmlns:xdt=""http://schemas.microsoft.com/XML-Document-Transform"">
+                  <system.net>
+                    <mailSettings>
+                      <smtp deliveryMethod=""Network"" from=""dan@test.com""
+                            xdt:Locator=""XPath(configuration/system.net/mailSettings/smtp
+                               [@deliveryMethod='SpecifiedPickupDirectory'])""
+                            xdt:Transform=""Replace"">
+                        <network host=""liveMailServer""/>
+                      </smtp>
+                    </mailSettings>
+                  </system.net>
+                </configuration>
+                ");
+
+            var output = new XdtTransformer().Transform(this.inputDoc, transformDoc);
+            Assert.IsTrue(XNode.DeepEquals(this.desiredDoc, output));
+        }
+    }
+}

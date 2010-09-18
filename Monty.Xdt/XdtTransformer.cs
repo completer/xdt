bfc3fd89f5ba785288e91ba82eb545eacaac2460
sumo.the.cat@gmail.com <sumo.the.cat@gmail.com@3a2e4736-875a-60fb-4bd3-e3a832ad883a>
2010-09-18 01:28:10 +0000

    tidy

--- Monty.Xdt/XdtTransformer.cs
+++ Monty.Xdt/XdtTransformer.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Linq;
-using System.Xml.XPath;
-using System.Text.RegularExpressions;
 
 namespace Monty.Xdt
 {

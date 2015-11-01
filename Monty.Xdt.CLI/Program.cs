31949ff7563e2ea85485322b3c30784a2e6fa0bb
ngeor <Nikolaos.Georgiou@gmail.com>
2015-11-01 08:25:38 +0100

    Adding CLI to be able to transform files on the command line.

---
+++ Monty.Xdt.CLI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Monty.Xdt.CLI
+{
+	/// <summary>
+	/// Utility that transforms config files on the command line.
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Gets or sets the source file.
+		/// </summary>
+		/// <value>The source file.</value>
+		public string SourceFile { get; set;  }
+
+		/// <summary>
+		/// Gets or sets the transform file.
+		/// </summary>
+		/// <value>The transform file.</value>
+		public string TransformFile { get; set; }
+
+		/// <summary>
+		/// Gets or sets the destination file.
+		/// </summary>
+		/// <value>The destination file.</value>
+		public string DestinationFile { get; set; }
+
+		/// <summary>
+		/// Transforms the source file using the transform file and writes the output into
+		/// destination file.
+		/// </summary>
+		protected void Run()
+		{
+			var sourceDocument = XDocument.Load(SourceFile);
+			var transformDocument = XDocument.Load(TransformFile);
+
+			var xdtTransformer = new XdtTransformer();
+
+			var destinationDocument = xdtTransformer.Transform(sourceDocument, transformDocument);
+
+			destinationDocument.Save(DestinationFile);
+		}
+
+		public static void Main(string[] args)
+		{
+			Program program = new Program
+				{
+					SourceFile = args[0],
+					TransformFile = args[1],
+					DestinationFile = args[2]
+				};
+			program.Run();
+		}
+	}
+}
